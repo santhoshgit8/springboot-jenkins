92128b5dd0c68cb77260049a4980deb69764fbf0
saud ahmad <ahmadsaud125@gmail.com>
2022-08-26 13:56:16 +0530

    first commit from springboot

---
+++ src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+		
+		 
+	}
+	@GetMapping("/exm")
+	public String example() {
+		return "welcome to india";
+	}
+
+}
